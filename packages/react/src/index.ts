ba42008cd37b0277799549fba61bf639a47cc291
Khôi Nguyễn <khoinpb96@gmail.com>
2025-09-04 14:43:32 +0700

    fix: correct import statement for Survey component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,5 @@
 import SurveyWidget from "@/survey-widget";
-import Survey from "@/survey";
+import { Survey } from "@/survey";
 import { surveyService } from "@repo/core";
 
 export { Survey, surveyService, SurveyWidget };
